ad503736869c202cc39d4452a402926391ffc25b
Yee404 <yee231193@uvg.edu.gt>
2023-07-25 13:22:36 -0600

    Reestructuración del mapa

--- game/MyWorld.java
+++ game/MyWorld.java
@@ -16,7 +16,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 1200, 1); 
+        super(1200, 780, 1); 
 
         prepare();
     }
@@ -29,65 +29,9 @@ public class MyWorld extends World
     {
         Ground ground = new Ground();
         addObject(ground,61,551);
-        Ground ground2 = new Ground();
-        addObject(ground2,355,490);
-        Ground ground3 = new Ground();
-        addObject(ground3,117,375);
-        Ground ground4 = new Ground();
-        addObject(ground4,622,432);
-        Ground ground5 = new Ground();
-        addObject(ground5,600,312);
-        Ground ground6 = new Ground();
-        addObject(ground6,318,248);
-        Ground ground7 = new Ground();
-        addObject(ground7,111,130);
-        Ground ground8 = new Ground();
-        addObject(ground8,628,170);
-        Ground ground9 = new Ground();
-        addObject(ground9,417,49);
         Player1 player1 = new Player1();
         addObject(player1,61,520);
         Cloud cloud = new Cloud();
         addObject(cloud,404,141);
-        Cloud cloud2 = new Cloud();
-        addObject(cloud2,697,65);
-        Cloud cloud3 = new Cloud();
-        addObject(cloud3,147,53);
-        Cloud cloud4 = new Cloud();
-        addObject(cloud4,706,246);
-        Cloud cloud5 = new Cloud();
-        addObject(cloud5,41,194);
-        Ground ground10 = new Ground();
-        addObject(ground10,594,609);
-        Ground ground11 = new Ground();
-        addObject(ground11,150,728);
-        Ground ground12 = new Ground();
-        addObject(ground12,456,850);
-        //Ground ground13 = new Ground();
-        //addObject(ground13,150,971);
-        Ground ground14 = new Ground();
-        addObject(ground14,131,1148);
-        Ground ground15 = new Ground();
-        addObject(ground15,295,1147);
-        Ground ground16 = new Ground();
-        addObject(ground16,293,1148);
-        Ground ground17 = new Ground();
-        addObject(ground17,627,727);
-        Ground ground18 = new Ground();
-        addObject(ground18,433,1029);
-        Ground ground19 = new Ground();
-        addObject(ground19,329,609);
-        Cloud cloud6 = new Cloud();
-        addObject(cloud6,20,889);
-        Cloud cloud7 = new Cloud();
-        addObject(cloud7,143,619);
-        Cloud cloud8 = new Cloud();
-        addObject(cloud8,767,881);
-        Cloud cloud9 = new Cloud();
-        addObject(cloud9,433,747);
-        player1.setLocation(143,1128);
-        end end = new end();
-        addObject(end,489,1153);
-        end.setLocation(127,1200);
     }
 }
