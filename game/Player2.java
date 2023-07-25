0a815cfb59361a6f4feb063e85343199f102ea62
Alejoram2505 <139787408+Alejoram2505@users.noreply.github.com>
2023-07-25 14:30:56 -0600

    inclusión de segundo jugador
    se agrego al segundo jugador y se cambiaron personajes

---
+++ game/Player2.java
@@ -0,0 +1,62 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class player1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Player2 extends Actor
+{
+    private int vSpeed = 0;
+    private int acceleration = 1;
+    private int jumpHeight= -20;
+    /**
+     * Act - do whatever the player1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public void act()
+    {
+        moveAround();
+        checkFalling();
+    }
+    private void fall()
+    {
+        setLocation(getX(), getY() + vSpeed);
+        vSpeed = vSpeed + acceleration;
+    }
+    public void moveAround()
+    {
+        if(Greenfoot.isKeyDown("D"))
+        {
+            move(4);
+        }
+        if(Greenfoot.isKeyDown("A"))
+        {
+            move(-4);
+        }
+        if (Greenfoot.isKeyDown("W")&&(onGround()==true))
+        {
+            vSpeed = jumpHeight;
+            fall();
+        }
+    }
+    boolean onGround()
+    {
+        Actor under = getOneObjectAtOffset(0, getImage().getHeight()/2, Ground.class);
+        return under != null;
+    }
+    public void checkFalling()
+    {
+        if (onGround()== false)
+        {
+            fall();
+        }
+        //if (onGround()== false)
+        //{
+        //    vSpeed = 0;
+        //}
+    }
+    
+}
